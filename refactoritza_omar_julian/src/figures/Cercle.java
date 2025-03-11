dea37301e16ea4d087b5f602c2c5260b08448ada
ojulian-daw <ojulian@inscaparrella.cat>
2025-03-11 19:07:26 +0100

    Add files via upload

--- refactoritza_omar_julian/src/figures/Cercle.java
+++ refactoritza_omar_julian/src/figures/Cercle.java
@@ -2,16 +2,25 @@ package figures;
 public class Cercle {  
 
 private double rad;  
-
+private double PI;
 public Cercle(double radi) {  
 this.rad = radi;  
+this.PI=3.1416;
 }  
+
+public double getPI() {
+	return PI;
+}
+
+public void setPI(double pI) {
+	PI = pI;
+}
 
 public void imprimir() {  
 String color = "vermell";  
 System.out.println("Diàmetre: " + 2 * rad);  
 System.out.println("Color: " + color);  
-double area = 2 * 3.1416 * rad * rad;  
+double area = 2 * PI * rad * rad;  
 System.out.println(area);  
 }  
 
